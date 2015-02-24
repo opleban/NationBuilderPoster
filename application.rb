2313e0c882a44630cbf65ce94849d4893b8bd696
Ori Pleban <opleban@gmail.com>
2015-02-24 14:55:59 -0500

    reconfigures headers'

--- application.rb
+++ application.rb
@@ -1,16 +1,18 @@
-require 'sinatra'
 require 'sinatra/base'
-require 'sinatra/cross_origin'
 require 'nationbuilder'
 
 class SkeletonNBApp < Sinatra::Base
 	register Sinatra::CrossOrigin
 
+	before do
+  		if request.request_method == 'OPTIONS'
+    		response.headers["Access-Control-Allow-Origin"] = "lacity-staging.demo.socrata.com"
+    		response.headers["Access-Control-Allow-Methods"] = "POST"
+    		halt 200
+		end
+	end
+
 	post '/people' do
-		cross_origin :allow_origin => 'https://lacity-staging.demo.socrata.com',
-		    :allow_methods => [:post],
-		    :allow_credentials => false,
-		    :max_age => "60"
 	    content_type :json
 	 
 	    nb_client = NationBuilder::Client.new('testnation', ENV['NB_API_KEY'])
