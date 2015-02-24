5e6034599a5e1dcdcca2d4a74b3cfb415d8f35c2
Ori Pleban <opleban@gmail.com>
2015-02-24 13:58:36 -0500

    adds CORS config

--- application.rb
+++ application.rb
@@ -5,7 +5,11 @@ require 'nationbuilder'
 class SkeletonNBApp < Sinatra::Base
 
 	post '/people' do
-		cross_origin
+		 cross_origin :allow_origin => 'https://lacity-staging.demo.socrata.com || https://lacity.data.org',
+		    :allow_methods => [:post],
+		    :allow_credentials => false,
+		    :max_age => "60"
+
 	    content_type :json
 	 
 	    nb_client = NationBuilder::Client.new('testnation', ENV['NB_API_KEY'])
