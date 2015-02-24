051267cd03322141351bce882c1433961cfed578
Ori Pleban <opleban@gmail.com>
2015-02-24 15:17:19 -0500

    last try

--- application.rb
+++ application.rb
@@ -6,21 +6,20 @@ class SkeletonNBApp < Sinatra::Base
 	register Sinatra::CrossOrigin
 	use Rack::Cors do
 		allow do
-			origins 'lacity-staging.demo.socrata.com', 'data.lacity.org'
+			origins '*'
 			resource '/people',
 				methods: [:post],
 			end
 		end
+
+		allow do 
+			origins '*'
+			resource '/people',
+			methods: [:post]
+		end
 	end
 
 	post '/people' do
-		before do
-	  		if request.request_method == 'OPTIONS'
-	    		response.headers["Access-Control-Allow-Origin"] = "https://lacity-staging.demo.socrata.com"
-	    		response.headers["Access-Control-Allow-Methods"] = "POST"
-	    		halt 200
-			end
-		end
 	    content_type :json
 	 
 	    nb_client = NationBuilder::Client.new('testnation', ENV['NB_API_KEY'])
